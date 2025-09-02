210d3287c263c2ccbe6f9052bff4daaa913d333a
ramtin.zand <ramtinzand6@gmail.com>
2025-09-02 23:30:14 +0330

    test7

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,9 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import express from "express";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// سرو کردن React Build
+// سرو کردن React build
 app.use(express.static(path.join(__dirname, "build")));
 
 // همه مسیرها به index.html هدایت شوند
@@ -15,4 +11,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-export default app;
+module.exports = app;
